3341e1ed4edd9f0a7b907c15b6b85a9995a147ee
Pramod Choyal <choyalpramod@gmail.com>
2023-03-21 06:37:16 +0100

    testing jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,9 +2,8 @@ def gv
 
 pipeline {
     agent any
-    parameters {
-        choice(name: 'VERSION', choices: ["1.2.0", "1.3.0", "1.4.0"], description: 'Version')
-        booleanParam(name: 'executeTests', defaultValue: true, description: 'Execute Test')
+    tools {
+        maven "Maven"
     }
     stages {
         stage("init") {
@@ -31,8 +30,6 @@ pipeline {
         stage("deploy") {
             steps {
                 script {
-                    env.ENV = input message: "Select a env to deploy", ok: "Done", parameters: [choice(name: 'ONE', choices: ["staging", "qa", "prod"], description: "choose env")]
-                    echo "env ${ENV}"
                     gv.deployApp()
                 }
             }
--- script.groovy
+++ script.groovy
@@ -1,22 +1,19 @@
 def buildJar() {
     echo "building the application..."
-    // sh 'mvn package'
+    sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    // withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    //     sh 'docker build -t choyalpramod/demo-app:jma-2.0 .'
-    //     sh "echo $PASS | docker login -u $USER --password-stdin"
-    //     sh 'docker push choyalpramod/demo-app:jma-2.0'
-    // }
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t choyalpramod/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push choyalpramod/demo-app:jma-2.0'
+    }
 } 
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "params: ${params.VERSION}"
-    echo "params: ${params}"
-    echo "${ENV}"
 } 
 
 return this
