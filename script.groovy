e8aaa9a00205bf36483e66356c48169b4ebe379e
Pramod Choyal <choyalpramod@gmail.com>
2023-04-04 14:43:46 +0200

    testing

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
 def buildJar() {
     echo "building the application..."
-    // sh 'mvn package'
+    sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     echo "${params.VERSION}"
-    // withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    //     sh 'docker build -t choyalpramod/demo-app:jma-2.0 .'
-    //     sh "echo $PASS | docker login -u $USER --password-stdin"
-    //     sh 'docker push choyalpramod/demo-app:jma-2.0'
-    // }
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t choyalpramod/demo-app:jma-${params.VERSION} ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push choyalpramod/demo-app:jma-${params.VERSION}"
+    }
 } 
 
 def deployApp() {
